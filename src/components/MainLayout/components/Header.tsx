66837e5e71465e4234ba87249479f863d01d3cca
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-03-14 22:58:40 +0200

    updated Readme

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My game Store - task2
+            My game Store - task2 updated
           </Link>
         </Typography>
 
