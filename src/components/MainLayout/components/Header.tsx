114dde1fa21cfcbc995c4de71e15bfcff896d0ed
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-03-14 22:50:51 +0200

    task 2 finished

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My game Store - task2
           </Link>
         </Typography>
 
