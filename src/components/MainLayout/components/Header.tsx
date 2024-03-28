83acb9c259db023870da8896bae70e07cf64a343
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-03-28 21:28:40 +0200

    updates for module 4

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store - task3
+            My Store - task4
           </Link>
         </Typography>
 
