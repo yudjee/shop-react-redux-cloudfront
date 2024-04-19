2f252b547d1a8ae6699dc212b4bd859089ab1d34
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-04-19 23:14:16 +0300

    finished task5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://qya0mkganc.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://f6y03uq1ff.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
